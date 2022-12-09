18fa14ef92173e6a6318c8a5c337ac322f2a3b7a
Samar7z <101511093+Samar7z@users.noreply.github.com>
2022-12-09 13:00:18 +0000

    Update employee.py

--- employee.py
+++ employee.py
@@ -2,14 +2,74 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name):
+    def _init_(self, name, contract, salary , hours, is_Commission , commision_Type , commission , contracts_Number ):
         self.name = name
+        self.contract = contract
+        self.salary = salary
+        self.hours = hours
+        self.is_Commission = is_Commission
+        self.commision_Type = commision_Type
+        self.commission = commission
+        self.contracts_Number = contracts_Number
+        
+        
 
     def get_pay(self):
-        pass
+        pay = 0
 
-    def __str__(self):
-        return self.name
+        if self.contract == "monthly":
+            if self.is_Commission == False:
+                pay = self.salary
+
+            elif self.is_Commission and self.commision_Type == "bonus":
+                pay = self.salary + self.commission
+
+            else:
+                pay = self.salary +  (self.contracts_Number * self.commission)
+
+        elif self.contract == "hourly":
+            if self.is_Commission == False:
+                pay = self.salary * self.hours
+
+            elif self.is_Commission and self.commision_Type == "bonus":
+                pay = (self.salary * self.hours) + self.commission
+
+            else:
+                pay = (self.salary * self.hours) +  (self.contracts_Number * self.commission)
+
+        return pay
+
+
+
+
+
+
+    def _str_(self):
+        describe = ""
+        
+        if self.contract == "monthly":
+            if self.is_Commission == False:
+                describe += f'{self.name} works on a monthly salary of {self.salary}.  Their total pay is {self.get_pay()}.'
+
+            elif self.is_Commission and self.commision_Type == "bonus":
+                describe += f'{self.name} works on a monthly salary of {self.salary} and receives a bonus commission of {self.commission}.  Their total pay is {self.get_pay()}.'
+
+            else:
+                describe += f'{self.name} works on a monthly salary of {self.salary} and receives a commission for {self.contracts_Number} contract(s) at {self.commission}/contract.  Their total pay is {self.get_pay()}.'
+
+
+        elif self.contract == "hourly":
+            if self.is_Commission == False:
+                describe += f'{self.name} works on a contract of {self.salary} hours at {self.hours}/hour.  Their total pay is {self.get_pay()}.'
+
+            elif self.is_Commission and self.commision_Type == "bonus":
+                describe += f'{self.name} works on a contract of {self.salary} hours at {self.hours}/hour and receives a bonus commission of {self.commission}.  Their total pay is {self.get_pay()}.'
+
+            else:
+               describe += f'{self.name} works on a contract of {self.salary} hours at {self.hours}/hour and receives a commission for {self.contracts_Number} contract(s) at {self.commission}/contract.  Their total pay is {self.get_pay()}.'                
+        
+        return describe
+
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
